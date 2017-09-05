daf27119fa8dbbbbf15f1d71eb6677a3a1abcf59
David Setz <david.setz@kaufhof.de>
2017-09-05 16:05:18 +0200

    Add some colour

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,16 @@
 /*  eslint-disable no-undef */
 const backgroundColor = [
-  'rgba(255, 99, 132, 0.2)',
-  'rgba(54, 162, 235, 0.2)',
-  'rgba(255, 206, 86, 0.2)',
-  'rgba(75, 192, 192, 0.2)',
-  'rgba(153, 102, 255, 0.2)',
-  'rgba(255, 159, 64, 0.2)',
+  'rgba(0,163,210, .6)',
+  'rgba(125,191,196, .6)',
+  'rgba(35,61,99, .6)',
+  'rgba(130,127,132, .6)',
 ];
 
 const borderColor = [
-  'rgba(255,99,132,1)',
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
+  'rgba(0,163,210, 1)',
+  'rgba(125,191,196, 1)',
+  'rgba(35,61,99, 1)',
+  'rgba(130,127,132, 1)',
 ];
 
 const chart2011 = {
@@ -114,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
       'animation.animateRotate': true,
       cutoutPercentage: 50,
     },
-    data: chart2011,
+    data: chart2017,
   });
 
   const updateChart = (data) => {
