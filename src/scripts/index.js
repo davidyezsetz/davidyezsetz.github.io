dc5474d16867ee46cbb3033de90fe26c62a4b5a0
David Setz <david.setz@kaufhof.de>
2017-09-05 15:37:04 +0200

    Add chart data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,132 @@
-/*  eslint-disable no-unused-vars, no-undef */
+/*  eslint-disable no-undef */
+const backgroundColor = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(255, 206, 86, 0.2)',
+  'rgba(75, 192, 192, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+  'rgba(255, 159, 64, 0.2)',
+];
+
+const borderColor = [
+  'rgba(255,99,132,1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+];
+
+const chart2011 = {
+  labels: ['CSS', 'JavaScript', 'PHP: WordPress, Symfony', 'Phonegap'],
+  datasets: [{
+    label: '# of Votes',
+    data: [30, 30, 20, 10],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2012 = {
+  labels: ['CSS', 'JavaScript', 'PHP: WordPress, Symfony', 'Phonegap'],
+  datasets: [{
+    label: '# of Votes',
+    data: [30, 40, 10, 20],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2013 = {
+  labels: ['CSS', 'JavaScript', 'PHP: WordPress', 'Ruby on Rails'],
+  datasets: [{
+    label: '# of Votes',
+    data: [35, 20, 35, 10],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2014 = {
+  /*  eslint-disable no-script-url */
+  labels: ['CSS', 'JavaScript: AngularJS, PhaserJS', 'PHP: WordPress', 'Ruby on Rails'],
+  datasets: [{
+    label: '# of Votes',
+    data: [40, 40, 10, 10],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2015 = {
+  /*  eslint-disable no-script-url */
+  labels: ['CSS', 'JavaScript: reactJS', 'PHP: WordPress', 'Ruby on Rails'],
+  datasets: [{
+    label: '# of Votes',
+    data: [40, 20, 20, 20],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2016 = {
+  /*  eslint-disable no-script-url */
+  labels: ['CSS', 'JavaScript: AngularJS, reactJS', 'Scala: Play'],
+  datasets: [{
+    label: '# of Votes',
+    data: [50, 40, 10],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
+const chart2017 = {
+  /*  eslint-disable no-script-url */
+  labels: ['CSS', 'JavaScript: AngularJS', 'Scala: Play'],
+  datasets: [{
+    label: '# of Votes',
+    data: [60, 30, 10],
+    backgroundColor,
+    borderColor,
+    borderWidth: 1,
+  }],
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const ctx = document.getElementById('myChart');
-  const myChart = new Chart(ctx, {
+  const ctx = document.getElementById('js_techChart');
+  const button2011 = document.getElementById('js_techChart2011');
+  const button2012 = document.getElementById('js_techChart2012');
+  const button2013 = document.getElementById('js_techChart2013');
+  const button2014 = document.getElementById('js_techChart2014');
+  const button2015 = document.getElementById('js_techChart2015');
+  const button2016 = document.getElementById('js_techChart2016');
+  const button2017 = document.getElementById('js_techChart2017');
+
+  const techChart = new Chart(ctx, {
     type: 'doughnut',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      }],
+    options: {
+      'animation.animateRotate': true,
+      cutoutPercentage: 50,
     },
-    options: {},
+    data: chart2011,
   });
+
+  const updateChart = (data) => {
+    techChart.data = data;
+    techChart.update(1000, false);
+  };
+
+  button2011.addEventListener('click', () => updateChart(chart2011));
+  button2012.addEventListener('click', () => updateChart(chart2012));
+  button2013.addEventListener('click', () => updateChart(chart2013));
+  button2014.addEventListener('click', () => updateChart(chart2014));
+  button2015.addEventListener('click', () => updateChart(chart2015));
+  button2016.addEventListener('click', () => updateChart(chart2016));
+  button2017.addEventListener('click', () => updateChart(chart2017));
 });
